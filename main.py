75ae55f34545d76898420131383e77b3006fe756
Anant Naithani <anant7738w@gmail.com>
2026-08-27 20:11:54 +0530

    made front end screen

---
+++ main.py
@@ -0,0 +1,8 @@
+with open("Fresh-Grow.txt", encoding="utf-8") as logo:
+    data = logo.read()
+    print(data)
+
+login = input("Please login (Enter Fresh-Grow ID)"
+      " {Type 'new' for a new customer} \n- ")
+
+
