2db452a465716422b6c9116faee8975003f09d68
Anant Naithani <anant7738w@gmail.com>
2026-08-29 00:32:36 +0530

    made new customer information csv file

--- customer.py
+++ customer.py
@@ -10,11 +10,17 @@ class NewCustomer:
         self.gender = input("Please input your gender[M/F] :")
         self.email = input("Please input your email address :")
         self.phone = input("Please input your phone number :")
-        new_customer_data = {"ID":["id number"],
-                             "Name":[self.name],
-                             "age":[self.age],
-                             "Gender":[self.gender],
-                             "Email":[self.email],
-                             "Phone":[self.phone]}
-        data = pandas.DataFrame(new_customer_data)
-        data.to_csv("cust-id.csv", mode="a")
+
+        data = pandas.read_csv("cust-id.csv", index_col=0)
+        id_length = len(data) + 1000
+        print(f"ID created successfully")
+        print(f"ID - {id_length}")
+
+        new_customer_row = pandas.DataFrame([{"Name":self.name,
+                                              "Age":self.age,
+                                              "Gender":self.gender,
+                                              "Email":self.email,
+                                              "Phone":self.phone}],
+                                               index=[id_length])
+        data = pandas.concat([data, new_customer_row])
+        data.to_csv("cust-id.csv")
